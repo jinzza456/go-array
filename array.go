d8522d20b87471eeb2cdb10204899b2f47246a0d
jinzza456 <jinzza456@gmail.com>
2021-10-13 10:23:48 +0900

    rune array

--- array.go
+++ array.go
@@ -16,9 +16,24 @@ func main() {
 	// 	fmt.Print(string(s[i]), ",")
 	// }
 
+	// s := "Hello 월드"
+
+	// for i := 0; i < len(s); i++ {
+	// 	fmt.Print(s[i], ",")
+	// }
+	// s := "Hello 월드"
+
+	// s2 := []rune(s)
+	// fmt.Println("len(s2) = ", len(s2))
+	// for i := 0; i < len(s2); i++ {
+	// 	fmt.Print(s2[i], ", ")
+	// }
+
 	s := "Hello 월드"
 
-	for i := 0; i < len(s); i++ {
-		fmt.Print(s[i], ",")
+	s2 := []rune(s)
+	fmt.Println("len(s2) = ", len(s2))
+	for i := 0; i < len(s2); i++ {
+		fmt.Print(string(s2[i]), ", ")
 	}
 }
