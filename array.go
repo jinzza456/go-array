7014cd7434699fcea92808c7853de29dffd86ee0
jinzza456 <jinzza456@gmail.com>
2021-10-13 12:00:30 +0900

    radix sort

--- array.go
+++ array.go
@@ -58,10 +58,27 @@ func main() {
 	// fmt.Println("temp:", temp)
 	// fmt.Println("arr:", arr)
 
-	arr := [5]int{1, 2, 3, 4, 5}
+	// arr := [5]int{1, 2, 3, 4, 5}
 
-	for i := 0; i < len(arr)/2; i++ {
-		arr[i], arr[len(arr)-1-i] = arr[len(arr)-1-i], arr[i]
-	} // arr[0],arr[4] = 5, 1
+	// for i := 0; i < len(arr)/2; i++ {
+	// 	arr[i], arr[len(arr)-1-i] = arr[len(arr)-1-i], arr[i]
+	// } // arr[0],arr[4] = 5, 1
+	// fmt.Println(arr)
+
+	arr := [11]int{0, 5, 4, 9, 5, 4, 3, 2, 1, 9, 5}
+	temp := [10]int{}
+
+	for i := 0; i < len(arr); i++ {
+		idx := arr[i] // arr[i] 에 해당하는 값을 idx로 한다
+		temp[idx]++   // 각 원소 들이 몇번 나오는지 횟수를 저장한다.
+	}
+
+	idx := 0                         // 현재 인덱스
+	for i := 0; i < len(temp); i++ { // temp의 갯수 10만큼 돈다
+		for j := 0; j < temp[i]; j++ { //temp에 저장된 숫자 = 갯수 이므로 그 숫자 만큼 반복한다.
+			arr[idx] = i
+			idx++ // idx 0 부터 증가시켜 숫자를 정렬 시킴
+		}
+	}
 	fmt.Println(arr)
 }
