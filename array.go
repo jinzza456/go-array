324301f7f01787cc7f1313ac4954396ca8364cbc
jinzza456 <jinzza456@gmail.com>
2021-10-13 10:08:25 +0900

    string array

--- array.go
+++ array.go
@@ -3,10 +3,22 @@ package main
 import "fmt"
 
 func main() {
-	var A [10]int
+	// var A [10]int
 
-	for i := 0; i < 10; i++ {
-		A[i] = i * i
+	// for i := 0; i < len(A); i++ {
+	// 	A[i] = i * i
+	// }
+	// fmt.Println(A)
+
+	// s := "Hello World"
+
+	// for i := 0; i < len(s); i++ {
+	// 	fmt.Print(string(s[i]), ",")
+	// }
+
+	s := "Hello 월드"
+
+	for i := 0; i < len(s); i++ {
+		fmt.Print(s[i], ",")
 	}
-	fmt.Println(A)
 }
